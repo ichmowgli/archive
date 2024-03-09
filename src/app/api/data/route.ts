cd1c58efb56c8f509adddee30bddd36f93defbcf
Mara K <mariia.kucheieva@gmail.com>
2024-03-09 20:23:22 +0200

    style: prettier

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -55,7 +55,7 @@ const data: Item[] = [
   {
     id: 7,
     title: 'Clever Dripper',
-    price: 34.90,
+    price: 34.9,
     currency: 'EUR',
     company: 'Clever',
     category: 'Coffee',
@@ -70,7 +70,7 @@ const data: Item[] = [
     company: 'Globe Trotter',
     category: 'Lifestyle',
     image: '/images/centenary.png',
-    href: 'https://www.globe-trotter.com/products/centenary-4-wheels-carry-on-black-black-black'
+    href: 'https://www.globe-trotter.com/products/centenary-4-wheels-carry-on-black-black-black',
   },
   {
     id: 9,
@@ -80,7 +80,7 @@ const data: Item[] = [
     company: 'Riot Division',
     category: 'Lifestyle',
     image: '/images/twist-lock-carabiner.png',
-    href: 'https://riotdivision.tech/products/twist-lock-carabiner-rd-tlc-grey'
+    href: 'https://riotdivision.tech/products/twist-lock-carabiner-rd-tlc-grey',
   },
   {
     id: 10,
@@ -90,7 +90,7 @@ const data: Item[] = [
     company: 'The District',
     category: 'Lifestyle',
     image: '/images/unfortunately.png',
-    href: 'https://the-brandidentity.com/store/product/unfortunately'
+    href: 'https://the-brandidentity.com/store/product/unfortunately',
   },
   {
     id: 11,
@@ -100,7 +100,7 @@ const data: Item[] = [
     company: 'Fellow',
     category: 'Coffee',
     image: '/images/ode.png',
-    href: 'https://fellowproducts.com/products/ode-brew-grinder-gen-2?variant=40978992463972'
+    href: 'https://fellowproducts.com/products/ode-brew-grinder-gen-2?variant=40978992463972',
   },
 ];
 
