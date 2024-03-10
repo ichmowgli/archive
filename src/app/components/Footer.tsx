6ae0815407f6adefafd6d72519e348b4006be0f8
Mara K <mariia.kucheieva@gmail.com>
2024-03-10 23:04:43 +0200

    style: footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,21 @@
+import { ArrowUpRight } from 'lucide-react';
+import Link from 'next/link';
+
 function Footer() {
   return (
-    <footer className="h-20 border-t border-gray-400 px-4 text-sm md:px-8">
-      <div className="flex h-full items-center justify-between">
-        <a href="https://mowgli.codes">Mara Kucheieva</a>
-        <a href="">Follow me on Twitter</a>
+    <footer className="m-3.5 flex flex-row items-center justify-between rounded-xl bg-white p-5 text-sm text-zinc-600	md:text-xs">
+      <div>
+        <span className="text-muted-foreground">Build by</span>{' '}
+        <Link
+          className="text-zinc-600 underline underline-offset-4 transition-colors duration-300 ease-in-out hover:text-primary"
+          href="https://mowgli.codes"
+        >
+          ichmowgli
+        </Link>
+      </div>
+      <div className="group flex flex-row items-center font-normal">
+        <Link href="https://twitter.com/ichmowgli">Follow me on Twitter</Link>
+        <ArrowUpRight size={20} className=" ml-1" />
       </div>
     </footer>
   );
