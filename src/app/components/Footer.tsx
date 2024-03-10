7f5d4fa53947853eb5811e11075d7204f33e1722
Mara K <mariia.kucheieva@gmail.com>
2024-03-10 14:07:22 +0200

    chore: basic footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="mt-5 h-20 border-t border-gray-400 px-4 text-sm md:px-8">
+      <div className="flex h-full items-center justify-between">
+        <a href="https://mowgli.codes">Mara Kucheieva</a>
+        <a href="">Follow me on Twitter</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
