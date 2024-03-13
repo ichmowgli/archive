0c8ad03ccda58afc1c51a6a5b6df2d15207be2c9
Mara K <mariia.kucheieva@gmail.com>
2024-03-13 17:52:07 +0200

    perf: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { GeistSans } from 'geist/font/sans';
 import type { Metadata } from 'next';
